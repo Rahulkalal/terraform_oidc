59b0813a5ad9a0612f9a242e846cf547f62fdcd2
Ubuntu <ubuntu@ip-172-31-31-234.ap-southeast-2.compute.internal>
2024-12-20 06:51:21 +0000

    rm

--- data.tf
+++
@@ -1,2 +0,0 @@
-# Data Source for Availability Zones
-data "aws_availability_zones" "available" {}
--- provider.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-  region = var.aws_region
-}
-
-# Variables
-variable "aws_region" {
-  description = "The AWS region to create resources"
-  default     = "us-east-1"  # Change this to your preferred region
-}
