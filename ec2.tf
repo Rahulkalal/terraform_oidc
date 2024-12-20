4dee56a4de4001caf313df766b177ebf627db7ed
Rahulkalal <107499198+Rahulkalal@users.noreply.github.com>
2024-12-20 12:12:04 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,19 +1,9 @@
-resource "aws_instance" "web" {
-  count         = 2
-  ami           = "ami-0e2c8caa4b6378d8c" # Amazon Linux 2 AMI
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.public[count.index].id
-  security_groups = [aws_security_group.web_sg.name]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              yum install -y httpd
-              systemctl start httpd
-              systemctl enable httpd
-            EOF
-
-  tags = {
-    Name = "WebServer-${count.index}"
-  }
+provider "aws" {
+  region = var.aws_region
 }
 
+# Variables
+variable "aws_region" {
+  description = "The AWS region to create resources"
+  default     = "us-east-1"  # Change this to your preferred region
+}
